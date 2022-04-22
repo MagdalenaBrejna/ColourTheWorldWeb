804bbc6337b2cf67a82a35cc64dcf36f5db64014
MagdalenaBrejna <magdalena.brejna@wp.pl>
2022-04-22 08:25:35 +0200

    feat: Create user authentication

---
+++ main.py
@@ -0,0 +1,155 @@
+#import os
+#import urllib.request
+
+from flask import Flask, flash, request, redirect, url_for, render_template
+from werkzeug.utils import secure_filename
+#from PIL import Image
+
+#import cv2
+
+#from flask import Flask,render_template,request,redirect
+from flask_login import login_required, current_user, login_user, logout_user
+from models import UserModel,db,login
+ 
+app = Flask(__name__)
+app.secret_key = 'xyz'
+ 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+ 
+ 
+db.init_app(app)
+login.init_app(app)
+login.login_view = 'login'
+ 
+@app.before_first_request
+def create_all():
+    db.create_all()
+     
+@app.route('/blogs')
+@login_required
+def blog():
+    return render_template('blog.html')
+ 
+ 
+@app.route('/login', methods = ['POST', 'GET'])
+def login():
+    if current_user.is_authenticated:
+        return redirect('/blogs')
+     
+    if request.method == 'POST':
+        email = request.form['email']
+        user = UserModel.query.filter_by(email = email).first()
+        if user is not None and user.check_password(request.form['password']):
+            login_user(user)
+            return redirect('/blogs')
+     
+    return render_template('login.html')
+ 
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    if current_user.is_authenticated:
+        return redirect('/blogs')
+     
+    if request.method == 'POST':
+        email = request.form['email']
+        username = request.form['username']
+        password = request.form['password']
+ 
+        if UserModel.query.filter_by(email=email).first():
+            return ('Email already Present')
+             
+        user = UserModel(email=email, username=username)
+        user.set_password(password)
+        db.session.add(user)
+        db.session.commit()
+        return redirect('/login')
+    return render_template('register.html')
+ 
+ 
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect('/blogs')
+
+if __name__ == '__main__':
+    app.run()   
+
+'''
+from flask import Blueprint
+#from . import db
+
+#from . import db
+
+main = Blueprint('main', __name__)
+
+@main.route('/')
+def index():
+    return 'Index'
+
+@main.route('/profile')
+def profile():
+    return 'Profile'
+'''
+'''
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
+def allowed_file(filename):
+	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+app = Flask(__name__)
+UPLOAD_FOLDER = 'static/uploads/'
+app.secret_key = "secret key"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
+class MyImage:
+    def __init__(self, name, img, author):
+        self.name = name
+        self.img = img
+        self.author = author
+
+class Images:
+    def __init__(self):
+        self.image_list = []        
+
+@app.route('/')
+def home():
+   return render_template("make.html")
+
+@app.route('/', methods=['POST'])
+def upload_image():
+	if 'file' not in request.files:
+		flash('No file part')
+		return redirect(request.url)
+	file = request.files['file']
+	if file.filename == '':
+		flash('No image selected for uploading')
+		return redirect(request.url)
+	if file and allowed_file(file.filename):
+		filename = secure_filename(file.filename)
+		file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+		image2 = cv2.imread(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+		image = cv2.cvtColor(image2 , cv2.COLOR_RGB2GRAY)
+		dst = cv2.GaussianBlur(image, (3, 3), cv2.BORDER_DEFAULT, 0.5)
+		laplacian = cv2.Laplacian(dst, -10, 3)
+		laplacian = 255 - laplacian
+
+		#edgesImage = cv2.Canny(image, 0.3, 0.8, 3)
+		data = Image.fromarray(laplacian)
+		data.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+		return render_template('make.html', filename=filename)
+	else:
+		flash('Allowed image types are -> png, jpg, jpeg, gif')
+		return redirect(request.url)   
+
+@app.route('/display/<filename>')
+def display_image(filename):
+	return redirect(url_for('static', filename='uploads/' + filename), code=301)        
+
+if __name__ == '__main__':
+    app.run()   
+
+'''
