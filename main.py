7505e7a4df1142d16e2a6c175251dba554fa7934
MagdalenaBrejna <magdalena.brejna@wp.pl>
2022-04-27 10:35:55 +0200

    feat: Add dropzone

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ from models import UserModel,db,login
 from PIL import Image
 import cv2
 
+from flask_dropzone import Dropzone
+dropzone = Dropzone()
+
+#configuration
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 UPLOAD_FOLDER = 'static/uploads/'
 
@@ -14,12 +18,16 @@ def allowed_file(filename):
 	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
  
 app = Flask(__name__)
+dropzone.init_app(app)
+
 app.secret_key = 'xyz'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+app.config['DROPZONE_MAX_FILES'] = 1
+
  
 db.init_app(app)
 login.init_app(app)
@@ -27,23 +35,9 @@ login.login_view = 'login'
  
 @app.before_first_request
 def create_all():
-    db.create_all()
-     
-
-@app.route('/projects')
-@login_required
-def projects():
-    return render_template('projects.html')	
-
-@app.route('/discover')
-def discover():
-    return render_template('discover.html')	
-
-@app.route('/create')
-@login_required
-def create():
-    return render_template('create.html')		 
+    db.create_all() 
  
+#security 
 @app.route('/login', methods = ['POST', 'GET'])
 def login():
     if current_user.is_authenticated:
@@ -84,10 +78,24 @@ def logout():
     logout_user()
     return redirect('/projects')
 
-
+#pages
 @app.route('/')
 def home():
    return render_template("make.html")	
+
+@app.route('/projects')
+@login_required
+def projects():
+    return render_template('projects.html')	
+
+@app.route('/discover')
+def discover():
+    return render_template('discover.html')	
+
+@app.route('/create')
+@login_required
+def create():
+    return render_template('create.html')		   
 	
 @app.route('/', methods=['POST'])
 def upload_image():
@@ -123,67 +131,3 @@ def display_image(filename):
 
 if __name__ == '__main__':
     app.run()   
-
-
-'''
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
-
-def allowed_file(filename):
-	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-app = Flask(__name__)
-UPLOAD_FOLDER = 'static/uploads/'
-app.secret_key = "secret key"
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
-
-class MyImage:
-    def __init__(self, name, img, author):
-        self.name = name
-        self.img = img
-        self.author = author
-
-class Images:
-    def __init__(self):
-        self.image_list = []        
-
-@app.route('/')
-def home():
-   return render_template("make.html")
-
-@app.route('/', methods=['POST'])
-def upload_image():
-	if 'file' not in request.files:
-		flash('No file part')
-		return redirect(request.url)
-	file = request.files['file']
-	if file.filename == '':
-		flash('No image selected for uploading')
-		return redirect(request.url)
-	if file and allowed_file(file.filename):
-		filename = secure_filename(file.filename)
-		file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-
-		image2 = cv2.imread(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-		image = cv2.cvtColor(image2 , cv2.COLOR_RGB2GRAY)
-		dst = cv2.GaussianBlur(image, (3, 3), cv2.BORDER_DEFAULT, 0.5)
-		laplacian = cv2.Laplacian(dst, -10, 3)
-		laplacian = 255 - laplacian
-
-		#edgesImage = cv2.Canny(image, 0.3, 0.8, 3)
-		data = Image.fromarray(laplacian)
-		data.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-
-		return render_template('make.html', filename=filename)
-	else:
-		flash('Allowed image types are -> png, jpg, jpeg, gif')
-		return redirect(request.url)   
-
-@app.route('/display/<filename>')
-def display_image(filename):
-	return redirect(url_for('static', filename='uploads/' + filename), code=301)        
-
-if __name__ == '__main__':
-    app.run()   
-
-'''
