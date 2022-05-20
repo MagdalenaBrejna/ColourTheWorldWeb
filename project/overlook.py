8320794151a803eaa6ab7eaf1c029df4edfa1b2d
MagdalenaBrejna <magdalena.brejna@wp.pl>
2022-05-20 20:07:00 +0200

    feat: Add displaying a community image gallery for a logged-in user

--- project/overlook.py
+++ project/overlook.py
@@ -6,7 +6,7 @@ from tkinter import *
 from flask_login import current_user
 import os
 
-# application module for colouring overlook
+# application module for colouring overlook and download
 
 # create look blueprint that is registered in the main module
 look = Blueprint('look', __name__)
@@ -24,15 +24,15 @@ def get_user_images():
     for mimg in allimages:
         if mimg.user == current_user.id:
             images2.append(convert_data(mimg.img, mimg.title))
-    return images2      
+    return images2 
 
 # save a user colouring book selected by a user into a selected directory
 @look.route('/get/<filename>')
 def download_user(filename):
     image = UserImageModel.query.filter_by(user=current_user.id, title=filename).first()
-    with open(os.path.join(getUserDirectoryPath(), filename), 'wb') as file:
-        file.write(image.img)
+    save_project(image, filename)
     return render_template('projects.html', images=get_user_images())
+
 
 
 # public projects overview request. It calls render proper tamplate with images stored in the database.
@@ -48,19 +48,38 @@ def get_published_images():
         images2.append(convert_data(mimg.img, mimg.title))
     return images2   
 
-# convert a given binary text from the database into a file
-def convert_data(data, file_name):
-    with open(file_name, 'wb') as file:
-        file.write(data)
-        return file.name
-
 # save a colouring book selected by a user into a selected directory
 @look.route('/download/<filename>')
 def download(filename):
     image = SharedImageModel.query.filter_by(title = filename).first()
+    save_project(image, filename)
+    return render_template('discover.html', images=get_published_images())
+
+
+
+# public projects overview request. It calls render proper tamplate with images stored in the database.
+@look.route('/published')
+def show_published():
+    return render_template('published.html', images=get_published_images())
+
+@look.route('/public/<filename>')
+def download_published(filename):
+    image = SharedImageModel.query.filter_by(title=filename).first()
+    save_project(image, filename)
+    return render_template('published.html', images=get_published_images())
+
+
+
+# convert a given binary text from the database into a file
+def convert_data(data, file_name):
+    with open(file_name, 'wb') as file:
+        file.write(data)
+        return file.name   
+
+# save a selected colouring book in a selected directory
+def save_project(image, filename):
     with open(os.path.join(getUserDirectoryPath(), filename), 'wb') as file:
         file.write(image.img)
-    return render_template('discover.html', images=get_published_images())
 
 # open a directory dialog to let user choose a place to save a colouring book
 def getUserDirectoryPath():
@@ -70,4 +89,3 @@ def getUserDirectoryPath():
     directory = filedialog.askdirectory()
     win.destroy()
     return directory
-  
