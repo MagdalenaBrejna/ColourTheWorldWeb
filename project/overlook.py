7e48e87eb9371eba74b9fd4b13fa7123bf2938d9
MagdalenaBrejna <magdalena.brejna@wp.pl>
2022-05-22 14:08:50 +0200

    reafactor: Clean code in the file overlook.html

--- project/overlook.py
+++ project/overlook.py
@@ -6,72 +6,72 @@ from tkinter import *
 from flask_login import current_user
 import os
 
-# application module for colouring overlook and download
+# application module for colouring books overlook and download
 
-# create look blueprint that is registered in the main module
+
+# create the look blueprint that is registered in the main module
 look = Blueprint('look', __name__)
 
-# projects overlook request. Send user saved colouring books to browser.
+# user projects overlook request. It sends user's saved in the database colouring books to the browser
 @look.route('/projects')
 @login_required
 def projects():
-    return render_template('projects.html', images=get_user_images())	
+    return render_template('projects.html', images = get_user_images())	
 
-# get user private coloring books stored in the database converted from binary into files
+# get user private coloring books stored in the database and conver them from binaries into files
 def get_user_images():
-    images2 = []
-    allimages = UserImageModel.query.all()
-    for mimg in allimages:
-        if mimg.user == current_user.id:
-            images2.append(convert_data(mimg.img, mimg.title))
-    return images2 
+    images = []
+    binary_images = UserImageModel.query.filter_by(user = current_user.id).all()
+    for image in binary_images:
+        images.append(convert_data(image.img, image.title))
+    return images 
 
-# save a user colouring book selected by a user into a selected directory
+# save a user colouring book selected by a user into a selected directory in computer files (download)
 @look.route('/get/<filename>')
 def download_user(filename):
-    image = UserImageModel.query.filter_by(user=current_user.id, title=filename).first()
+    image = UserImageModel.query.filter_by(user = current_user.id, title = filename).first()
     save_project(image, filename)
-    return render_template('projects.html', images=get_user_images())
+    return render_template('projects.html', images = get_user_images())
 
 
 
-# public projects overview request. It calls render proper tamplate with images stored in the database.
+# public projects overview request. It sends community saved in the database colouring books to the browser (discover.html)
 @look.route('/discover')
 def discover():
-    return render_template('discover.html', images=get_published_images())	
+    return render_template('discover.html', images = get_published_images())	
 
-# get public coloring books stored in the database converted from binary into files
+# get public coloring books stored in the database and convert them from binaries into files
 def get_published_images():
-    images2 = []
-    allimages = SharedImageModel.query.all()
-    for mimg in allimages:
-        images2.append(convert_data(mimg.img, mimg.title))
-    return images2   
+    images = []
+    binary_images = SharedImageModel.query.all()
+    for image in binary_images:
+        images.append(convert_data(image.img, image.title))
+    return images   
 
-# save a colouring book selected by a user into a selected directory
+# save a colouring book selected by a user into a selected directory in computer files (download)
 @look.route('/download/<filename>')
 def download(filename):
     image = SharedImageModel.query.filter_by(title = filename).first()
     save_project(image, filename)
-    return render_template('discover.html', images=get_published_images())
+    return render_template('discover.html', images = get_published_images())
 
 
 
-# public projects overview request. It calls render proper tamplate with images stored in the database.
+# public projects overview request. It sends community saved in the database colouring books to the browser ("published.html")
 @look.route('/published')
 def show_published():
-    return render_template('published.html', images=get_published_images())
+    return render_template('published.html', images = get_published_images())
 
-# save a colouring book selected by a user into a selected directory
+# save a colouring book selected by a user into a selected directory in computer files (download)
 @look.route('/public/<filename>')
 def download_published(filename):
-    image = SharedImageModel.query.filter_by(title=filename).first()
+    image = SharedImageModel.query.filter_by(title = filename).first()
     save_project(image, filename)
-    return render_template('published.html', images=get_published_images())
+    return render_template('published.html', images = get_published_images())
 
 
 
-# save a colouring book into a selected directory
+# save an unsaved and unpublished colouring book into a selected directory in computer files (download)
 @look.route('/saveproject', methods=['POST'])
 def download_project():
     image = request.form.get("image_to_download")
@@ -79,7 +79,7 @@ def download_project():
         img = file.read()
     with open(os.path.join(getUserDirectoryPath(), image[1:]), 'wb') as file:
         file.write(img)
-    return render_template('created.html', filename=image[1:])
+    return render_template('created.html', filename = image[1:])
 
 
 
